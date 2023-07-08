34b657eb50e5096676629edf0584ea538d4f2f63
Judah <judahben149@gmail.com>
2023-07-08 23:52:54 +0100

    Initial commit

---
+++ app/src/main/java/com/judahben149/talknotes/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.judahben149.talknotes
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Mic
+import androidx.compose.material.icons.rounded.Stop
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.judahben149.talknotes.ui.theme.TalkNotesTheme
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+class MainActivity : ComponentActivity() {
+
+    val parser by lazy {
+        VoiceToTextParser(application)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            var canRecord by remember {
+                mutableStateOf(false)
+            }
+
+            val recordAudioLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = { isGranted ->
+                    canRecord = isGranted
+                }
+            )
+
+            LaunchedEffect(key1 = recordAudioLauncher) {
+                recordAudioLauncher.launch(Manifest.permission.RECORD_AUDIO)
+            }
+
+            val state by parser.state.collectAsState()
+
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            if (state.isSpeaking) {
+                                parser.stopListening()
+                            } else parser.startListening()
+                        }
+                    ) {
+                        AnimatedContent(targetState = state.isSpeaking) { isSpeaking ->
+                            if (isSpeaking) {
+                                Icon(imageVector = Icons.Rounded.Stop, contentDescription = null)
+                            } else {
+                                Icon(imageVector = Icons.Rounded.Mic, contentDescription = null)
+                            }
+                        }
+                    }
+                }
+            ) { paddingValues ->
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(20.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    AnimatedContent(targetState = state.isSpeaking) { isSpeaking ->
+                        if (isSpeaking) {
+                            Text(text = "Speaking...")
+                        } else {
+                            Text(text = state.spokenText.ifEmpty { "Click on mic to record audio" })
+                        }
+                    }
+                }
+            }
+
+            TalkNotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
